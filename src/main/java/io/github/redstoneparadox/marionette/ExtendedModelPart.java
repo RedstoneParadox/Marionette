8c6a45294cf19d9f6f945b27d274e7629b0fe7e6
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-08 11:49:35 -0700

    ExtendedModelPart is complete for now

---
+++ src/main/java/io/github/redstoneparadox/marionette/ExtendedModelPart.java
@@ -0,0 +1,56 @@
+package io.github.redstoneparadox.marionette;
+
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A {@link ModelPart} with some additional properties.
+ */
+public class ExtendedModelPart extends ModelPart {
+	public float scaleX = 1.0f;
+	public float scaleY = 1.0f;
+	public float scaleZ = 1.0f;
+	public float red = 1.0f;
+	public float green = 1.0f;
+	public float blue = 1.0f;
+	public float alpha = 1.0f;
+
+	public ExtendedModelPart(Model model) {
+		super(model);
+	}
+
+	public ExtendedModelPart(Model model, int textureOffsetU, int textureOffsetV) {
+		super(model, textureOffsetU, textureOffsetV);
+	}
+
+	public ExtendedModelPart(int textureWidth, int textureHeight, int textureOffsetU, int textureOffsetV) {
+		super(textureWidth, textureHeight, textureOffsetU, textureOffsetV);
+	}
+
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+		if (this.visible) {
+			if (!cuboids.isEmpty() || !children.isEmpty()) {
+				matrices.push();
+				rotate(matrices);
+				matrices.scale(scaleX, scaleY, scaleZ);
+
+				renderCuboids(matrices.peek(), vertices, light, overlay, this.red, this.blue, this.green, this.alpha);
+
+				for (ModelPart child: children) {
+					if (child instanceof ExtendedModelPart) {
+						child.render(matrices, vertices, light, overlay);
+					}
+					else {
+						child.render(matrices, vertices, light, overlay, 1.0f, 1.0f, 1.0f, 1.0f);
+					}
+				}
+			}
+		}
+	}
+}
