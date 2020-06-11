6ca0cbc833910290a239b135d7ecf656593a1f0e
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-11 12:14:01 -0700

    SineSampler now throws an IllegalStateException when the time goes out of bounds

--- src/main/java/io/github/redstoneparadox/marionette/animation/sampling/SineSampler.java
+++ src/main/java/io/github/redstoneparadox/marionette/animation/sampling/SineSampler.java
@@ -21,10 +21,13 @@ public class SineSampler extends Sampler {
 		KeyFramePair pair = getFrames(time);
 		KeyFrame first = pair.first;
 		KeyFrame second = pair.second;
-
 		float x = (time - first.getTime())/(second.getTime() - first.getTime());
-		float y = (float) ((Math.sin(Math.PI * x - Math.PI/2) + 1)/2);
 
+		if (x < 0 || x > 1){
+			throw new IllegalStateException("Expected x value between 0 and 1, instead got " + x + ".");
+		}
+
+		float y = (float) ((Math.sin(Math.PI * x - Math.PI/2) + 1)/2);
 		float delta = second.getValue() - first.getValue();
 		return (y * delta) + first.getValue();
 	}
