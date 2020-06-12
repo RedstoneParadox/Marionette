3f7f0c17243407c385f9e434dc115c79408058e7
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-12 11:31:51 -0700

    Deleted test code and all render code

--- src/main/java/io/github/redstoneparadox/marionette/test/MarionetteTestClient.java
+++ src/main/java/io/github/redstoneparadox/marionette/test/MarionetteTestClient.java
@@ -1,14 +1,10 @@
 package io.github.redstoneparadox.marionette.test;
 
-import io.github.redstoneparadox.marionette.test.entity.TestEntity;
-import io.github.redstoneparadox.marionette.test.render.TestEntityModel;
-import io.github.redstoneparadox.marionette.test.render.TestEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 
 public class MarionetteTestClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		EntityRendererRegistry.INSTANCE.register(TestEntity.TYPE, ((dispatcher, context) -> new TestEntityRenderer(dispatcher, new TestEntityModel(), 1.0f)));
+
 	}
 }
