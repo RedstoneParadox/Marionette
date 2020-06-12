e902a75124fe4c705b44c7c18b027e064f348b19
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-11 20:58:53 -0700

    Laying groundwork for animation state machine

---
+++ src/main/java/io/github/redstoneparadox/marionette/animation/AnimationState.java
@@ -0,0 +1,21 @@
+package io.github.redstoneparadox.marionette.animation;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class AnimationState {
+	private final List<AbstractAnimation> animations = new ArrayList<>();
+	private final Set<AnimationState> states = new HashSet<>();
+
+	public void addAnimation(AbstractAnimation animation) {
+		animations.add(animation);
+	}
+
+	public void linkState(AnimationState state) {
+		states.add(state);
+	}
+
+	
+}
