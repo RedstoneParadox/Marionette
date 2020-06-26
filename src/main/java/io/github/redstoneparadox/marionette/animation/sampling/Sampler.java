ffdd5993fab4fbde581ec9c764621c9516e7dd87
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-26 15:08:40 -0700

    API methods should appear above non-api ones

--- src/main/java/io/github/redstoneparadox/marionette/animation/sampling/Sampler.java
+++ src/main/java/io/github/redstoneparadox/marionette/animation/sampling/Sampler.java
@@ -20,6 +20,17 @@ public abstract class Sampler {
 		this.keyFrames = keyFrames;
 	}
 
+	/**
+	 * Called to sample a value between two keyframes.
+	 *
+	 * @param totalTime The time between the two keyframes.
+	 * @param deltaTime The time after the first keyframe to sample at.
+	 * @param first The first keyframe value.
+	 * @param second The second keyframe value.
+	 * @return The interpolated value between the two keyframes.
+	 */
+	abstract float sample(float totalTime, float deltaTime, float first, float second);
+
 	@ApiStatus.Internal
 	public final float sample(float time) {
 		if (time <= 0.0f) {
@@ -35,17 +46,6 @@ public abstract class Sampler {
 
 		return sample(totalTime, deltaTime, first.getValue(), second.getValue());
 	}
-
-	/**
-	 * Called to sample a value between two keyframes.
-	 *
-	 * @param totalTime The time between the two keyframes.
-	 * @param deltaTime The time after the first keyframe to sample at.
-	 * @param first The first keyframe value.
-	 * @param second The second keyframe value.
-	 * @return The interpolated value between the two keyframes.
-	 */
-	abstract float sample(float totalTime, float deltaTime, float first, float second);
 
 	private Pair<KeyFrame, KeyFrame> getFrames(float time) {
 		int frameIndex = 0;
